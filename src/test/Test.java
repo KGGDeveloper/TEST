d815ef003b41d6135110775503cc248c1b95395e
cougarlj <cougarlj@gmail.com>
2013-07-18 21:46:27 +0800

    Second Try! Good Luck!

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,5 +16,6 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("I Love You!");
+        System.out.print("Second Try 9:45PM");
     }
 }
